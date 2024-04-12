465c42eb7691a47230836604d2450e9526493469
daGoodenough <dgoodenough@proton.me>
2024-04-12 08:15:11 -0700

    add nix

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Dev environment for BBB NWC Workshop";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            nativeBuildInputs = [ pkgs.bun pkgs.just ];
+            shellHook = ''
+              bun install
+            '';
+          };
+        };
+      });
+}
